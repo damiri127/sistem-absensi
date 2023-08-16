7fe220f66e3e3cba8bde5ecd4813caa7a9fe3b5a
anantapk03 <anantapk03@gmail.com>
2023-08-16 22:48:42 +0700

    view Dashboard admin

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,18 +1,114 @@
 @extends('layout.admin')
 @section('content')
+
+{{-- Panel Header --}}
 <div class="panel-header bg-primary-gradient">
     <div class="page-inner py-5">
         <div class="d-flex align-items-left align-items-md-center flex-column flex-md-row">
             <div>
-                <h2 class="text-white pb-2 fw-bold">Dashboard</h2>
-                <h5 class="text-white op-7 mb-2">Free Bootstrap 4 Admin Dashboard</h5>
+                <h2 class="text-white pb-2 fw-bold">Selamat Datang, Ananta!</h2>
+                <h5 class="text-white op-7 mb-2">Sistem  Absensi Guru SMKS Teladan Kertasmaya</h5>
             </div>
             <div class="ml-md-auto py-2 py-md-0">
-                <a href="#" class="btn btn-white btn-border btn-round mr-2">Manage</a>
-                <a href="#" class="btn btn-secondary btn-round">Add Customer</a>
+                <a href="#" class="btn btn-white btn-border btn-round mr-2">Informasi Absensi</a>
+                <a href="#" class="btn btn-secondary btn-round">Data Pengguna</a>
             </div>
         </div>
     </div>
 </div>
+{{-- End Panel Header --}}
+
+{{-- Informasi --}}
+<div class="page-inner mt-2">
+    <div>
+        <div class="card full-height">
+            <div class="card-body">
+                <div class="card-title">Aktivitas Admin</div>
+                <div class="card-category">Selasa, 20 September 2023 - Pukul 08.00</div>
+                <div class="mt-3">
+                    <div class="row">
+						<div class="col-sm-6 col-md-3">
+							<div class="card card-stats card-primary card-round">
+								<div class="card-body">
+									<div class="row">
+										<div class="col-5">
+											<div class="icon-big text-center">
+												<i class="flaticon-users"></i>
+											</div>
+										</div>
+										<div class="col-7 col-stats">
+											<div class="numbers">
+												<p class="card-category">Informasi Pengguna</p>
+												<h4 class="card-title">1,294</h4>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-sm-6 col-md-3">
+							<div class="card card-stats card-info card-round">
+								<div class="card-body">
+									<div class="row">
+										<div class="col-5">
+											<div class="icon-big text-center">
+												<i class="flaticon-interface-6"></i>
+											</div>
+										</div>
+										<div class="col-7 col-stats">
+											<div class="numbers">
+												<p class="card-category">Informasi Absensi</p>
+												<h4 class="card-title">1303</h4>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-sm-6 col-md-3">
+							<div class="card card-stats card-success card-round">
+								<div class="card-body ">
+									<div class="row">
+										<div class="col-5">
+											<div class="icon-big text-center">
+												<i class="flaticon-analytics"></i>
+											</div>
+										</div>
+										<div class="col-7 col-stats">
+											<div class="numbers">
+												<p class="card-category">Jadwal Belajar</p>
+												<h4 class="card-title">100</h4>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-sm-6 col-md-3">
+							<div class="card card-stats card-secondary card-round">
+								<div class="card-body ">
+									<div class="row">
+										<div class="col-5">
+											<div class="icon-big text-center">
+												<i class="flaticon-success"></i>
+											</div>
+										</div>
+										<div class="col-7 col-stats">
+											<div class="numbers">
+												<p class="card-category">Laporan Absensi</p>
+												<h4 class="card-title">576</h4>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+{{-- End Informasi --}}
     
 @endsection
